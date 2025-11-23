d01b6888cd81d691bc960fb462f6dad89019e9a0
Young Wook Choi <37619106+B477042@users.noreply.github.com>
2025-11-23 13:58:47 +0900

    2025 11 23　エディターモジュール追加

---
+++ Source/ProjectileSmapleEditor/ProjectileSampleEditor.cpp
@@ -0,0 +1,6 @@
+﻿#include "ProjectileSampleEditor.h"
+#include "Modules/ModuleManager.h"
+
+
+
+IMPLEMENT_MODULE(FProjectileSampleEditorModule, ProjectileSampleEditor);
---
+++ Source/ProjectileSmapleEditor/ProjectileSampleEditor.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+class FProjectileSampleEditorModule : public IModuleInterface
+{
+	public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+	
+};
