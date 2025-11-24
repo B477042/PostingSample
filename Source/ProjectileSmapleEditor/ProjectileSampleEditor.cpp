5451d19220280536c6a6ddbbc042edcc64aa8469
Young Wook Choi <37619106+B477042@users.noreply.github.com>
2025-11-24 12:24:41 +0900

    20251124 uproject 掃除

--- Source/ProjectileSmapleEditor/ProjectileSampleEditor.cpp
+++ Source/ProjectileSmapleEditor/ProjectileSampleEditor.cpp
@@ -1,6 +1,14 @@
 ﻿#include "ProjectileSampleEditor.h"
 #include "Modules/ModuleManager.h"
 
+void FProjectileSampleEditorModule::StartupModule()
+{
+	IModuleInterface::StartupModule();
+}
 
+void FProjectileSampleEditorModule::ShutdownModule()
+{
+	IModuleInterface::ShutdownModule();
+}
 
 IMPLEMENT_MODULE(FProjectileSampleEditorModule, ProjectileSampleEditor);
